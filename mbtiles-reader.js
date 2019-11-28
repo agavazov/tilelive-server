dd4879299b872d2e8ee2f3919b1625964c28e5e3
Alexander Gavazov <alex@studio.bg>
2019-11-28 14:22:23 +0200

    Reformat the code and add readme information

--- mbtiles-reader.js
+++ mbtiles-reader.js
@@ -9,9 +9,8 @@ var url = require('url');
 
 // Load tilelive
 var npmLocation = './node_modules/';
-if (!fs.existsSync(npmLocation))
-{
-	npmLocation = '/usr/lib/node_modules/';
+if (!fs.existsSync(npmLocation)) {
+    npmLocation = '/usr/lib/node_modules/';
 }
 
 // Protobuf
@@ -25,130 +24,107 @@ var sqlite3 = require(npmLocation + 'sqlite3').verbose();
 var db = new sqlite3.Database(mbTilesFile);
 
 var app = {
-	port: port,
+    port: port,
 
-	start: function ()
-	{
-		var server = http.createServer(function (request, response)
-		{
-			var query = url.parse(request.url, true).query;
+    start: function () {
+        var server = http.createServer(function (request, response) {
+            var query = url.parse(request.url, true).query;
 
-			if (query.zoom)
-			{
-				query.zoom = parseInt(query.zoom);
-			}
+            if (query.zoom) {
+                query.zoom = parseInt(query.zoom);
+            }
 
-			try
-			{
-				if (!query.type)
-				{
-					app.showMap(request, response);
-				}
-				else if (query.type && query.x && query.y && query.zoom)
-				{
-					query.x  = 9223;
-					query.y = 10440;
-					query.zoom = 14;
+            try {
+                if (!query.type) {
+                    app.showMap(request, response);
+                } else if (query.type && query.x && query.y && query.zoom) {
+                    query.x = 9223;
+                    query.y = 10440;
+                    query.zoom = 14;
 
-					try
-					{
-						db.serialize(function ()
-						{
-							var sql = 'SELECT "tile_data" FROM "main"."tiles" WHERE "zoom_level" = "' + query.zoom + '" AND "tile_column" = "' + query.x + '" AND "tile_row" = "' + query.y + '"';
-							// var sql = 'SELECT "tile_data" FROM "main"."tiles" WHERE "zoom_level" = "' + query.zoom + '" AND "tile_column" = "' + query.x + '" LIMIT 1';
-							console.log("\n\n" + sql + "\n\n");
+                    try {
+                        db.serialize(function () {
+                            var sql = 'SELECT "tile_data" FROM "main"."tiles" WHERE "zoom_level" = "' + query.zoom + '" AND "tile_column" = "' + query.x + '" AND "tile_row" = "' + query.y + '"';
+                            // var sql = 'SELECT "tile_data" FROM "main"."tiles" WHERE "zoom_level" = "' + query.zoom + '" AND "tile_column" = "' + query.x + '" LIMIT 1';
+                            console.log("\n\n" + sql + "\n\n");
 
-							db.get(sql, function (err, row)
-							{
-								if (err)
-								{
-									app.showError(request, response, err);
-									return;
-								}
+                            db.get(sql, function (err, row) {
+                                if (err) {
+                                    app.showError(request, response, err);
+                                    return;
+                                }
 
-								if (!row)
-								{
-									app.showError(request, response, 'No results found');
-									return;
-								}
+                                if (!row) {
+                                    app.showError(request, response, 'No results found');
+                                    return;
+                                }
 
-								var tile = row.tile_data;
+                                var tile = row.tile_data;
 
-								var headers = {
-									'Content-Type': 'application/x-protobuf',
-									'x-tilelive-contains-data': true,
-									'Content-Encoding': 'gzip',
-									'Access-Control-Allow-Origin': '*',
-									'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-								};
+                                var headers = {
+                                    'Content-Type': 'application/x-protobuf',
+                                    'x-tilelive-contains-data': true,
+                                    'Content-Encoding': 'gzip',
+                                    'Access-Control-Allow-Origin': '*',
+                                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+                                };
 
-								response.writeHead(200, headers);
+                                response.writeHead(200, headers);
 
-								// response.write(tile);
-								// response.end();
+                                // response.write(tile);
+                                // response.end();
 
-								// @todo remove me
-								zlib.gunzip(tile, function (err, dezipped)
-								{
-									var parsed = pMessages.Tile.decode(dezipped);
+                                zlib.gunzip(tile, function (err, dezipped) {
+                                    var parsed = pMessages.Tile.decode(dezipped);
 
-									console.log(JSON.stringify(parsed));
+                                    console.log(JSON.stringify(parsed));
 
-									var pbf = pMessages.Tile.encode(parsed);
+                                    var pbf = pMessages.Tile.encode(parsed);
 
-									zlib.gzip(pbf, function (err, zipped)
-									{
-										response.write(zipped);
-										response.end();
-									});
-								});
-							});
-						});
-					}
-					catch (err)
-					{
-						app.showError(request, response, err);
-					}
+                                    zlib.gzip(pbf, function (err, zipped) {
+                                        response.write(zipped);
+                                        response.end();
+                                    });
+                                });
+                            });
+                        });
+                    } catch (err) {
+                        app.showError(request, response, err);
+                    }
 
-				}
-				else
-				{
-					app.showError(request, response, 'Page not found');
-				}
-			}
-			catch (err)
-			{
-				app.showError(request, response, 'ERROR::EXCEPTION - ' + err);
-			}
-		});
+                } else {
+                    app.showError(request, response, 'Page not found');
+                }
+            } catch (err) {
+                app.showError(request, response, 'ERROR::EXCEPTION - ' + err);
+            }
+        });
 
-		server.listen(app.port);
+        server.listen(app.port);
 
-		console.log('Server running at http://127.0.0.1:' + app.port);
-	},
+        console.log('Server running at http://127.0.0.1:' + app.port);
+    },
 
-	showMap: function (request, response)
-	{
-		var content = fs.readFileSync(__dirname + '/lib/map_demo.html').toString();
-		content = content.replace(/\%HOST\%/gi, request.headers.host);
+    showMap: function (request, response) {
+        var content = fs.readFileSync(__dirname + '/lib/map_demo.html').toString();
+        content = content.replace(/\%HOST\%/gi, request.headers.host);
 
-		response.writeHead(200, {
-			'Content-Type': 'text/html'
-		});
-		response.write(content);
-		response.end();
-	},
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        response.write(content);
+        response.end();
+    },
 
-	showError: function (request, response, error)
-	{
-		console.log('\033[31m' + 'ERROR' + '\033[39m', error);
+    showError: function (request, response, error) {
+        console.log('\033[31m' + 'ERROR' + '\033[39m', error);
 
-		response.writeHead(500, {
-			'Content-Type': 'text/plain'
-		});
-		response.write(JSON.stringify(error));
-		response.end();
-	}
+        response.writeHead(500, {
+            'Content-Type': 'text/plain'
+        });
+        response.write(JSON.stringify(error));
+        response.end();
+    }
 };
 
 app.start();
--- server.js
+++ server.js
@@ -16,9 +16,8 @@ var url = require('url');
 
 // Load tilelive
 var npmLocation = './node_modules/';
-if (!fs.existsSync(npmLocation))
-{
-	npmLocation = '/usr/lib/node_modules/';
+if (!fs.existsSync(npmLocation)) {
+    npmLocation = '/usr/lib/node_modules/';
 }
 
 // Protobuf & zlib
@@ -32,89 +31,76 @@ require(npmLocation + 'tilelive-bridge').registerProtocols(tilelive);
 
 // App package
 var app = {
-	start: function ()
-	{
-		var server = http.createServer(function (request, response)
-		{
-			var query = url.parse(request.url, true).query;
+    start: function () {
+        var server = http.createServer(function (request, response) {
+            var query = url.parse(request.url, true).query;
 
-			if (request.url == '/')
-			{
-				app.showIndex(request, response);
-				return;
-			}
+            if (request.url == '/') {
+                app.showIndex(request, response);
+                return;
+            }
 
-			try
-			{
-				query.zoom = parseInt(query.zoom);
+            try {
+                query.zoom = parseInt(query.zoom);
 
-				tilelive.load('bridge://' + requireXml, function (err, source)
-				{
-					if (err)
-					{
-						app.showError(request, response, err);
-						return;
-					}
+                tilelive.load('bridge://' + requireXml, function (err, source) {
+                    if (err) {
+                        app.showError(request, response, err);
+                        return;
+                    }
 
-					source.getTile(query.zoom, query.x, query.y, function (err, tile, headers)
-					{
-						if (err)
-						{
-							app.showError(request, response, err);
-							return;
-						}
+                    source.getTile(query.zoom, query.x, query.y, function (err, tile, headers) {
+                        if (err) {
+                            app.showError(request, response, err);
+                            return;
+                        }
 
-						headers['Access-Control-Allow-Origin'] = '*';
-						headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
+                        headers['Access-Control-Allow-Origin'] = '*';
+                        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
 
-						response.writeHead(200, headers);
+                        response.writeHead(200, headers);
 
-						response.write(tile);
-						response.end();
+                        response.write(tile);
+                        response.end();
 
-						// Debug
-						zlib.gunzip(tile, function (err, dezipped)
-						{
-							var parsed = pMessages.Tile.decode(dezipped);
-							console.log("\n\n\n", query.zoom, query.x, query.y);
-							console.log(JSON.stringify(parsed));
-						});
-					});
-				});
-			}
-			catch (err)
-			{
-				app.showError(request, response, err);
-			}
-		});
+                        // Debug
+                        zlib.gunzip(tile, function (err, dezipped) {
+                            var parsed = pMessages.Tile.decode(dezipped);
+                            console.log("\n\n\n", query.zoom, query.x, query.y);
+                            console.log(JSON.stringify(parsed));
+                        });
+                    });
+                });
+            } catch (err) {
+                app.showError(request, response, err);
+            }
+        });
 
-		server.listen(port);
+        server.listen(port);
 
-		console.log('Server running at http://127.0.0.1:' + port);
-	},
+        console.log('Server running at http://127.0.0.1:' + port);
+    },
 
-	showIndex: function (request, response)
-	{
-		var content = fs.readFileSync(__dirname + '/index.html').toString();
-		content = content.replace(/\%HOST\%/gi, request.headers.host);
+    showIndex: function (request, response) {
+        var content = fs.readFileSync(__dirname + '/index.html').toString();
+        content = content.replace(/\%HOST\%/gi, request.headers.host);
 
-		response.writeHead(200, {
-			'Content-Type': 'text/html'
-		});
-		response.write(content);
-		response.end();
-	},
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        response.write(content);
+        response.end();
+    },
 
-	showError: function (request, response, error)
-	{
-		console.log('\033[31m' + 'ERROR' + '\033[39m', error);
+    showError: function (request, response, error) {
+        console.log('\033[31m' + 'ERROR' + '\033[39m', error);
 
-		response.writeHead(500, {
-			'Content-Type': 'text/plain'
-		});
-		response.write(JSON.stringify(error));
-		response.end();
-	}
+        response.writeHead(500, {
+            'Content-Type': 'text/plain'
+        });
+        response.write(JSON.stringify(error));
+        response.end();
+    }
 };
 
 app.start();
